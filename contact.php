cc76d410e25059af65ccd54c014a2cc31fbe86d9
Lee71559 <lee71559@gmail.com>
2021-04-07 12:08:52 +0800

    Testing contact

--- contact.php
+++ contact.php
@@ -20,7 +20,7 @@ if (!$comment) $errors[count($errors)] = 'Please enter your message.';
 if (!$errors) {
 
 	//recipient - replace your email here
-	$to = 'wowthemesnet@gmail.com';	
+	$to = 'lee71559@gmail.com';	
 	//sender - from the form
 	$from = $name . ' <' . $email . '>';
 	
